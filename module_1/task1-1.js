2b3745b08b37e5525c512beef85e5ad97540c9cc
Gaikie Zhou <gaikie_zhou@epam.com>
2022-05-27 17:53:01 +0800

    fixed module1 homework

---
+++ module_1/task1-1.js
@@ -0,0 +1,9 @@
+var arr = [1, 2, 3, 4, 5, , 5, 7, 8];
+// document.write('要输出的内容', arr);
+console.log(arr);
+console.log(arr.reverse());
+
+var string = 'test data';
+// document.write('要输出的内容', arr);
+console.log(string);
+console.log(string.split("").reverse().join(""));
