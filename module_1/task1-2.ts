00d93bf15c2a5e22d7dd92a5c4692996f6451c9c
Gaikie Zhou <gaikie_zhou@epam.com>
2022-06-06 17:04:55 +0800

    fixed module_2

--- module_1/task1-2.ts
+++ module_1/task1-2.ts
@@ -1,14 +1,11 @@
-const {
-  log
-} = require('console');
-const fs = require('fs');
-const XLSX = require("xlsx");
+import fs from 'fs'
+import XLSX from 'xlsx'
+
 
 const CSV_FILE_PATH = "./data/nodejs-hw1-ex1.xlsx"
 
 const workbook = {}
 var jsonFile = XLSX.readFile(CSV_FILE_PATH)
-console.log(jsonFile)
 jsonFile.SheetNames.forEach(sheet => {
   workbook[sheet] = XLSX.utils.sheet_to_json(jsonFile.Sheets[sheet])
 })
@@ -32,5 +29,3 @@ for (const key in workbook) {
   });
 
 }
-
-// XLSX.writeFile('./data/jsonFile.json', jsonFile)
